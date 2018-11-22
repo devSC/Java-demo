19b3772a2e7c02aa5bb70884be0f8de76d024981
Wilson <xiaochong2154@163.com>
2018-11-22 18:50:44 +0800

    UPDATE

---
+++ HelloWorld/src/com/wilson/ScheduleExample.java
@@ -0,0 +1,59 @@
+package com.wilson;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduleExample {
+    final static DateFormat fmt = DateFormat.getTimeInstance(DateFormat.LONG);
+    static Integer times = 0;
+    static ScheduledFuture<?> periodicfFuture;
+
+    public static void main(String[] args) {
+        ScheduledThreadPoolExecutor sch = new ScheduledThreadPoolExecutor(5);
+
+        Runnable oneShotTask = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("\t oneShotTask Execution time: " + fmt.format(new Date()));
+            }
+        };
+
+        Runnable delayTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("\t delayTask Execution Time: " + fmt.format(new Date()));
+                    Thread.sleep(10 * 1000);
+                    System.out.println("\t delayTask End Time: " + fmt.format(new Date()));
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        Runnable periodicTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("\t  Execute times: " + times.toString() + " periodicTask Execution Time: " + fmt.format(new Date()));
+                    times += 1;
+                    Thread.sleep(3 * 1000);
+                    System.out.println("\t periodicTask End Time: " + fmt.format(new Date()));
+                    if (times > 3 && periodicfFuture != null) {
+                        periodicfFuture.cancel(false);
+                        System.out.println("\t periodicTask cancelled: " + fmt.format(new Date()));
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        };
+        System.out.println("Submission Time: " + fmt.format(new Date()));
+//        ScheduledFuture<?> oneShotFuture = sch.schedule(oneShotTask, 5, TimeUnit.SECONDS);
+//        ScheduledFuture<?> delayFuture = sch.scheduleWithFixedDelay(delayTask, 5, 5, TimeUnit.SECONDS);
+        periodicfFuture = sch.scheduleAtFixedRate(periodicTask, 5, 5, TimeUnit.SECONDS);
+    }
+}
